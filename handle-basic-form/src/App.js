8896c62e576cdc62ae1233c4377a1e1a937ad8dc
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-04-25 18:56:12 +0530

    handle basic form

--- handle-basic-form/src/App.js
+++ handle-basic-form/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
-
+import { useState } from "react";
 function App() {
+
+  const [Name, setName] = useState("");
+  const [tnc, setTnc] = useState(false);
+  const [interest, setInterest] = useState("");
+  const getFormData=(event)=>{
+    console.warn(Name,tnc,interest);
+    event.preventDefault();
+  }
   return (
     <div className="App">
-     
+     <h1>Handle form in react</h1>
+     <form onSubmit={getFormData}>
+     <input type="text" placeholder='Enter Name ' onChange={(e)=>{setName(e.target.value)}} /><br /><br />
+     <select onChange={(e)=>{setInterest(e.target.value)}}>
+      <option>Select Options</option>
+      <option>Marvel</option>
+      <option>DC</option>
+     </select>
+     <br /><br />
+     <input type="checkbox" onChange={(e)=>{setTnc(e.target.checked)}} /><span>Accept terms and conditions!</span>
+     <br /><br />
+     <button type="submit">Submit</button>
+     </form>
     </div>
   );
 }
