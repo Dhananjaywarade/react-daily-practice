e47689ad0de6bbbc481991a571d14e6783bc0d60
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-04-25 19:15:55 +0530

    add conditional rendering logic

--- conditional-rendering/src/App.js
+++ conditional-rendering/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [loggedIn, setLoggedIn] = useState(true)
   return (
     <div className="App">
       <h1>conditional rendering</h1>
+        {
+          loggedIn?<h1>welcome dhananjay</h1>:<h1>welcome guest</h1>
+        }
     </div>
   );
 }
