ff97c452253fcf93099b5cd497f9d5d76e7c8d35
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-05-16 17:28:04 +0530

    create function handleReset and also add toastify notification

--- form-submission/src/App.js
+++ form-submission/src/App.js
@@ -8,55 +8,78 @@ function App() {
   const [contact, setContact] = useState();
   const [gender, setGender] = useState("male");
   const [subjects, setSubjects] = useState({
-    english:true,
-    maths:false,
-    physics:false
+    english: true,
+    maths: false,
+    physics: false
   })
-  const [resume, setResume] = useState(); 
+  const [resume, setResume] = useState();
+  const [url, setUrl] = useState();
+  const [selectedOption, setSelectedOption] = useState("");
+  const [about, setAbout] = useState("");
 
-
-
-  const handleSubjectChange=(sub)=>{
-    setSubjects((prev)=>({
+  const handleSubjectChange = (sub) => {
+    setSubjects((prev) => ({
       ...prev,
-      [sub]:!prev[sub],
+      [sub]: !prev[sub],
     }));
   }
+
+  const handleReset=()=>{
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setContact("");
+    setGender("male");
+    setSubjects({
+      english: true,
+      maths: false,
+      physics: false
+    });
+    setResume("");
+    setUrl();
+    setSelectedOption();
+    setAbout();
+
+    toast("🔄 Form Reset Successful")
+  }
+
+
+
   return (
     <div className="App">
       <h1>Form React</h1>
       <form action="">
         <label htmlFor="firstName">First Name*</label>
-        <input type="text" name='firstName' value={firstName}  placeholder='Enter First Name' required onChange={(e)=>{setFirstName(e.target.value)}} />
+        <input type="text" name='firstName' value={firstName} placeholder='Enter First Name' required onChange={(e) => { setFirstName(e.target.value) }} />
         <label htmlFor="lastName">Last Name*</label>
-        <input type="text" name='lastName' value={lastName} placeholder='Enter Last Name' required onChange={(e)=>{setLastName(e.target.value)}}/>
+        <input type="text" name='lastName' value={lastName} placeholder='Enter Last Name' required onChange={(e) => { setLastName(e.target.value) }} />
         <label htmlFor="email">Email*</label>
-        <input type="email" name='email' value={email} placeholder='Enter Email' required onChange={(e)=>{setEmail(e.target.value)}}/>
+        <input type="email" name='email' value={email} placeholder='Enter Email' required onChange={(e) => { setEmail(e.target.value) }} />
         <label htmlFor="contact">Contact*</label>
-        <input type="tel" name="contact" value={contact} placeholder='Enter Mobile Number' required onChange={(e)=>{setContact(e.target.value)}}/>
+        <input type="tel" name="contact" value={contact} placeholder='Enter Mobile Number' required onChange={(e) => { setContact(e.target.value) }} />
 
         <label htmlFor="gender">Gender*</label>
-        <input type="radio" name="gender" id="" value="male" checked={gender==='male'} onChange={(e)=>{setGender(e.target.value)}} />Male
-        <input type="radio" name="gender" id="" value="female" checked={gender==='female'} onChange={(e)=>{setGender(e.target.value)}}/>Female
-        <input type="radio" name="gender" id="" value="other" checked={gender==='other'} onChange={(e)=>{setGender(e.target.value)}}/>Other
+        <input type="radio" name="gender" id="" value="male" checked={gender === 'male'} onChange={(e) => { setGender(e.target.value) }} />Male
+        <input type="radio" name="gender" id="" value="female" checked={gender === 'female'} onChange={(e) => { setGender(e.target.value) }} />Female
+        <input type="radio" name="gender" id="" value="other" checked={gender === 'other'} onChange={(e) => { setGender(e.target.value) }} />Other
 
         <label htmlFor="lang">Your Best Subject</label>
-        <input type="checkbox" name="lang" id="" value="english" checked={subjects.english===true} onChange={(e)=>{handleSubjectChange('english')}}/>English
-        <input type="checkbox" name="lang" id="" value="maths" checked={subjects.maths===true} onChange={(e)=>{handleSubjectChange('maths')}}/>Maths
-        <input type="checkbox" name="lang" id="" value="physics" checked={subjects.physics===true} onChange={(e)=>{handleSubjectChange('physics')}}/>Physics
+        <input type="checkbox" name="lang" id="" value="english" checked={subjects.english === true} onChange={(e) => { handleSubjectChange('english') }} />English
+        <input type="checkbox" name="lang" id="" value="maths" checked={subjects.maths === true} onChange={(e) => { handleSubjectChange('maths') }} />Maths
+        <input type="checkbox" name="lang" id="" value="physics" checked={subjects.physics === true} onChange={(e) => { handleSubjectChange('physics') }} />Physics
 
         <label htmlFor="resume">Upload Resume*</label>
-        <input type="file" name="resume" id="" onChange={(e)=>{setResume(e.target.value);toast.success("Resume Uploaded 📄");}}/>
+        <input type="file" name="resume" id="" onChange={(e) => { setResume(e.target.files[0]); toast.success("Resume Uploaded 📄"); }} />
 
         <label htmlFor="url">Enter URL*</label>
-        <input type="url" name="url" id="" />
+        <input type="url" name="url" id="" onChange={(e) => { setUrl(e.target.value) }} />
 
         <label >Select Your Choice</label>
-        <select name="select" id="">
+        <select name="select" id="" value={selectedOption} onChange={(e)=>{setSelectedOption(e.target.value);}}>
           <option
             value=""
             disabled
-
+            selected={selectedOption===""}
           >
             Select your Ans
           </option>
@@ -87,11 +110,13 @@ function App() {
           rows="10"
           placeholder="About your self"
           required
+          onChange={(e)=>{setAbout(e.target.value)}}
         ></textarea>
 
         <button
           type="reset"
           value="reset"
+          onClick={handleReset}
         >
           Reset
         </button>
