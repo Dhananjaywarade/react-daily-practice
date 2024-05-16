198e56c3c1f5ea0093f702e50bd0d0a02067d0e1
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-05-16 16:23:40 +0530

    add all logic

--- form-submission/src/App.js
+++ form-submission/src/App.js
@@ -1,31 +1,51 @@
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [contact, setContact] = useState();
+  const [gender, setGender] = useState("male");
+  const [subjects, setSubjects] = useState({
+    english:true,
+    maths:false,
+    physics:false
+  })
+  const [resume, setResume] = useState(); 
+
+
+
+  const handleSubjectChange=(sub)=>{
+    setSubjects((prev)=>({
+      ...prev,
+      [sub]:!prev[sub],
+    }));
+  }
   return (
     <div className="App">
       <h1>Form React</h1>
       <form action="">
         <label htmlFor="firstName">First Name*</label>
-        <input type="text" name='firstName' placeholder='Enter First Name' required />
+        <input type="text" name='firstName' value={firstName}  placeholder='Enter First Name' required onChange={(e)=>{setFirstName(e.target.value)}} />
         <label htmlFor="lastName">Last Name*</label>
-        <input type="text" name='lastName' placeholder='Enter Last Name' required />
+        <input type="text" name='lastName' value={lastName} placeholder='Enter Last Name' required onChange={(e)=>{setLastName(e.target.value)}}/>
         <label htmlFor="email">Email*</label>
-        <input type="email" name='email' placeholder='Enter Email' required />
+        <input type="email" name='email' value={email} placeholder='Enter Email' required onChange={(e)=>{setEmail(e.target.value)}}/>
         <label htmlFor="contact">Contact*</label>
-        <input type="tel" name="contact" placeholder='Enter Mobile Number' required />
+        <input type="tel" name="contact" value={contact} placeholder='Enter Mobile Number' required onChange={(e)=>{setContact(e.target.value)}}/>
 
         <label htmlFor="gender">Gender*</label>
-        <input type="radio" name="gender" id="" value="male" />Male
-        <input type="radio" name="gender" id="" value="Female" />Female
-        <input type="radio" name="gender" id="" value="Other" />Other
+        <input type="radio" name="gender" id="" value="male" checked={gender==='male'} onChange={(e)=>{setGender(e.target.value)}} />Male
+        <input type="radio" name="gender" id="" value="female" checked={gender==='female'} onChange={(e)=>{setGender(e.target.value)}}/>Female
+        <input type="radio" name="gender" id="" value="other" checked={gender==='other'} onChange={(e)=>{setGender(e.target.value)}}/>Other
 
         <label htmlFor="lang">Your Best Subject</label>
-        <input type="checkbox" name="lang" id="" value="english" />English
-        <input type="checkbox" name="lang" id="" value="maths" />Maths
-        <input type="checkbox" name="lang" id="" value="physics" />Physics
+        <input type="checkbox" name="lang" id="" value="english" checked={subjects.english===true} onChange={(e)=>{handleSubjectChange('english')}}/>English
+        <input type="checkbox" name="lang" id="" value="maths" checked={subjects.maths===true} onChange={(e)=>{handleSubjectChange('maths')}}/>Maths
+        <input type="checkbox" name="lang" id="" value="physics" checked={subjects.physics===true} onChange={(e)=>{handleSubjectChange('physics')}}/>Physics
 
         <label htmlFor="resume">Upload Resume*</label>
-        <input type="file" name="resume" id="" />
+        <input type="file" name="resume" id="" onChange={(e)=>{setResume(e.target.value);console.log("file upload successfully");}}/>
 
         <label htmlFor="url">Enter URL*</label>
         <input type="url" name="url" id="" />
@@ -59,7 +79,7 @@ function App() {
         </select>
 
 
-        <label for="about">About</label>
+        <label htmlFor="about">About</label>
         <textarea
           name="about"
           cols="30"
