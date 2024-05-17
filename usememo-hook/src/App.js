d9682feb9e597ad9a5b55f10db9921f2e3b2804b
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-05-17 14:01:40 +0530

    to enhance performance we use useMemo

--- usememo-hook/src/App.js
+++ usememo-hook/src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
-import { useState } from "react";
+import { useMemo, useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const [item, setItem] = useState(10);
 
-  
-  function updateCount() {
+  const multiCountMemo = useMemo(function updateCount() {
     console.log("update Memo");
-   return count*5;
-  }
-  
+    return count * 5;
+  }, [count]);
+
+
+
   return (
     <div className="App">
 
       <h1>Count : {count}</h1>
       <h1>Item : {item}</h1>
-    <h1>Update count : {updateCount()}</h1>
-      <button onClick={()=>{setCount(count+1)}}>Update Count</button>
-      <button onClick={()=>{setItem(item*10)}}>Update Item</button>
+      <h1>Update count : {multiCountMemo}</h1>
+      <button onClick={() => { setCount(count + 1) }}>Update Count</button>
+      <button onClick={() => { setItem(item * 10) }}>Update Item</button>
     </div>
   );
 }
