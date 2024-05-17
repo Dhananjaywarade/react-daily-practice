f4d245a36028a4c27b4d12c023dc2bc16f203d04
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-05-17 15:28:02 +0530

    By using useref we can manipulate dom directly eg.focus,get current value,change style

--- useref-hook/src/App.js
+++ useref-hook/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-
+import { useRef } from "react";
 function App() {
+  const inputRef = useRef(null);
+  const handleInput=()=>{
+    inputRef.current.value="5000";
+    inputRef.current.focus();
+  }
   return (
     <div className="App">
-     
+     <h1>UseRef In ReactJs</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleInput}>Handle Input</button>
     </div>
   );
 }
