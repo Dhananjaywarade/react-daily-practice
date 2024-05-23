6d4dd39f5e9fa62803e4f77eb4d82c9296c8f582
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-05-23 14:19:13 +0530

    by using axios : fetch all the information

--- crud-operations/src/App.js
+++ crud-operations/src/App.js
@@ -1,11 +1,34 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect,useState } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((data)=>{setData(data.data)})
+  }, [])
+  
   return (
     
     <div className="App">
-      
+      <table>
+        <tr>
+          <td>id</td>
+          <td>title</td>
+          <td>description</td>
+          <td>Image</td>
+        </tr>
+        {
+          data.map((item,key)=>{
+           return <tr key={key}>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+              <td><img style={{width:"50px"}} src={item.image} alt="" /></td>
+            </tr>
+          })
+        }
+      </table>
     </div>
   );
 }
