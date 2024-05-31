bc37e3fc1b0abf66d169060806f921fa362c0a53
Dhananjaywarade <waradedhananjay2000@gmail.com>
2024-05-31 11:47:21 +0530

    Write a code to retrieve previous data

--- previous-state/src/App.js
+++ previous-state/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-
+import { useState } from "react";
 function App() {
+
+  const [value, setValue] = useState(1);
+  const handleState=()=>{
+    let rand=Math.floor(Math.random(value)*10);
+    setValue((prev)=>{
+      console.log(prev);
+      return rand;
+    });
+  }
   return (
     <div className="App">
      
+     <h1>{value}</h1>
+     <button onClick={handleState}>update </button>
+
+
     </div>
   );
 }
